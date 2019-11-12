ec229d9e40d9dd1006e83785a7ca81082714bbd6
Tim Hor <timothy.hor@outlook.com>
2019-11-13 00:44:00 +1100

    Rename fields in Task model

--- src/tasks/Task.java
+++ src/tasks/Task.java
@@ -13,20 +13,20 @@ import javafx.beans.property.StringProperty;
  * @author jenniferruan
  */
 public class Task {
-    private String taskID; 
-    private StringProperty taskName; 
-    private StringProperty taskDescription; 
-    private StringProperty taskPriority; 
-    private StringProperty doDate; 
-    private StringProperty dueDate; 
-    
-    public Task(String taskID, String taskName, String taskDescription, String taskPriority, String doDate, String dueDate) {
-        this.taskID = taskID; 
-        this.taskName = new SimpleStringProperty(taskName);
-        this.taskDescription = new SimpleStringProperty(taskDescription);
-        this.taskPriority = new SimpleStringProperty(taskPriority);
-        this.doDate = new SimpleStringProperty(doDate);
+    private String taskID;
+    private StringProperty title;
+    private StringProperty description;
+    private StringProperty priority;
+    private StringProperty dueDate;
+    private StringProperty doDate;
+
+    public Task(String taskID, String title, String description, String priority, String dueDate, String doDate) {
+        this.taskID = taskID;
+        this.title = new SimpleStringProperty(title);
+        this.description = new SimpleStringProperty(description);
+        this.priority = new SimpleStringProperty(priority);
         this.dueDate = new SimpleStringProperty(dueDate);
+        this.doDate = new SimpleStringProperty(doDate);
     }
 
     public String getTaskID() {
@@ -37,45 +37,44 @@ public class Task {
         this.taskID = taskID;
     }
 
-    public StringProperty getTaskName() {
-        return taskName;
+    public StringProperty getTitleProperty() {
+        return title;
     }
 
-    public void setTaskName(StringProperty taskName) {
-        this.taskName = taskName;
+    public void setTitleProperty(StringProperty title) {
+        this.title = title;
     }
 
-    public StringProperty getTaskDescription() {
-        return taskDescription;
+    public StringProperty getDescriptionProperty() {
+        return description;
     }
 
-    public void setTaskDescription(StringProperty taskDescription) {
-        this.taskDescription = taskDescription;
+    public void setDescriptionProperty(StringProperty description) {
+        this.description = description;
     }
 
-    public StringProperty getTaskPriority() {
-        return taskPriority;
+    public StringProperty getPriorityProperty() {
+        return priority;
     }
 
-    public void setTaskPriority(StringProperty taskPriority) {
-        this.taskPriority = taskPriority;
+    public void setPriorityProperty(StringProperty priority) {
+        this.priority = priority;
     }
 
-    public StringProperty getDoDate() {
+    public StringProperty getDoDateProperty() {
         return doDate;
     }
 
-    public void setDoDate(StringProperty doDate) {
+    public void setDoDateProperty(StringProperty doDate) {
         this.doDate = doDate;
     }
 
-    public StringProperty getDueDate() {
+    public StringProperty getDueDateProperty() {
         return dueDate;
     }
 
-    public void setDueDate(StringProperty dueDate) {
+    public void setDueDateProperty(StringProperty dueDate) {
         this.dueDate = dueDate;
     }
-    
-    
+
 }
