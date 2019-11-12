3a8e766b973d81b7476962c1d1137c26d3ac63c0
Jeffrey <chanjeffrey9@gmail.com>
2019-11-12 23:06:35 +1100

    piechart

--- src/myLife/MyLifeScreenController.java
+++ src/myLife/MyLifeScreenController.java
@@ -1,16 +1,85 @@
 package myLife;
 
+import entries.EntriesScreenController;
+import entries.Entry;
+import static entries.Entry.parseTimeInMs;
+import helper.Database;
+
 import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.chart.Chart;
+import javafx.scene.chart.PieChart;
 import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
 import layout.LayoutScreenController;
 
 public class MyLifeScreenController {
 
+    @FXML
+    public PieChart pieChart;
+
+    @FXML
+    public Pane pane;
+
     LayoutScreenController layoutController = new LayoutScreenController();
 
-    // Side bar 
+    @FXML
+    public void initialize() {
+
+        //EntriesScreenController entriesScreenControllerObject = new EntriesScreenController();
+        //List categoryNames = new ArrayList();
+        //ArrayList entries = new ArrayList<Entry>();
+        try {
+
+            HashMap<String, Float> data = new HashMap<String, Float>();
+
+            String categorySt = "SELECT DISTINCT c.categoryname FROM entries e LEFT JOIN categories c ON e.category = c.id";
+            ResultSet rs = Database.getResultSet(categorySt);
+            while (rs.next()) {
+                data.put(rs.getString("categoryname"), 0.0f);
+            }
+
+            String timeSt = "SELECT c.categoryname, e.starttime, e.endtime FROM entries e LEFT JOIN categories c ON e.category = c.id";
+            ResultSet timeRs = Database.getResultSet(timeSt);
+            long total; 
+            
+            while (timeRs.next()) {
+                // calculate the time it takes 
+                String startTime = timeRs.getString("starttime");
+                String endTime = timeRs.getString("endtime");
+                long duration = parseTimeInMs(endTime) - parseTimeInMs(startTime);
+
+                // append to the hashmap
+                data.put(timeRs.getString("categoryname"), data.get(timeRs.getString("categoryname")) + duration);
+                System.out.println(data);
+            }
+            ArrayList<PieChart.Data> pieChartData = new ArrayList<>();
+
+            for (Map.Entry<String, Float> entry : data.entrySet()) {
+                String cat = entry.getKey();
+
+                Float duration = entry.getValue();
+
+                pieChartData.add(new PieChart.Data(cat, duration));
+
+            }
+            final PieChart pieChart = new PieChart(FXCollections.observableArrayList(pieChartData));
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
     @FXML
     public Button homeScreenButton;
 
@@ -22,7 +91,7 @@ public class MyLifeScreenController {
 
     @FXML
     public Button myWeekScreenButton;
-    
+
     @FXML
     public Button weeklyTrendsScreenButton;
 
@@ -32,7 +101,7 @@ public class MyLifeScreenController {
 
     @FXML
     public Button tasksScreenButton;
-    
+
     @FXML
     public Button aboutScreenButton;
 
@@ -57,7 +126,7 @@ public class MyLifeScreenController {
     public void handleHomeScreenButtonAction(ActionEvent event) throws IOException {
         layoutController.handleHomeScreenButtonAction(event);
     }
-    
+
     @FXML
     public void handleMyLifeScreenButtonAction(ActionEvent event) throws IOException {
         layoutController.handleMyLifeScreenButtonAction(event);
