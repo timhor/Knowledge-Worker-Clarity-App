1817046b4b139db36d4455ecf9e6be938df50b19
Jeffrey <chanjeffrey9@gmail.com>
2019-11-14 19:29:37 +1100

    semi fixed color coding
    colour --> color

--- src/myLife/MyLifeScreenController.java
+++ src/myLife/MyLifeScreenController.java
@@ -119,24 +119,30 @@ public class MyLifeScreenController {
                 long duration = parseTimeInMs(endTime) - parseTimeInMs(startTime);
                 float durationInHours = duration / 360000.0f;
                 totalTimeSpent += durationInHours;
+
                 // append to the hashmap
                 data.put(timeRs.getString("categoryname"), data.get(timeRs.getString("categoryname")) + durationInHours);
-            } 
-                        
+            }
             for (Map.Entry<String, Float> entry : data.entrySet()) {
                 String key = entry.getKey();
                 Float value = entry.getValue();
+                System.out.println(key);
+                System.out.println(value);
                 // jeff: % of time spent = value / totalTimeSpent * 100. so try and add the label here
                Float valuePercentage = 100*value / totalTimeSpent;
                DecimalFormat df = new DecimalFormat("0"); 
                String valuePercentageString = String.valueOf(df.format(valuePercentage));
-                //ngl cant tell if this is fkn retarded or im a genius 
                 myPieChart.getData().add(new PieChart.Data(key + "\n " + valuePercentageString + "%" , value / totalTimeSpent));
             }
             
-            ArrayList<String> colourCodes = new ArrayList<>();
+            
+            
+            
+            // here you'll probably add the color code work (i think). make sure you do it in a try/catch.
+            // you'll need to do a sql query to get the colours from the categories table 
+            // IDK why it doesnt work, the data is coming in properly and ive changed it in pie terms as well.. 
             try {
-                
+                ArrayList<String> colourCodes = new ArrayList<>();
                 for (Map.Entry<String, Float> entry : data.entrySet()) {
                     String key = entry.getKey();
                     String colourSt = "SELECT DISTINCT c.hexString FROM entries e LEFT JOIN categories c ON e.category = c.id WHERE c.categoryname = '" + key + "'";
@@ -147,42 +153,23 @@ public class MyLifeScreenController {
                         System.out.println(categoryColour);
                     }
                 }
-                System.out.println(colourCodes);
-                try {
-                    Node n = myPieChart.lookup(".data0.chart-pie");
-                    for (int i = 0; i < colourCodes.size(); i++){
-                        if (i != 0){
-                            n = myPieChart.lookup(".data" + i + ".chart-pie");
-                            System.out.println(".data" + i + ".chart-pie");
-                        }
-                        System.out.println(colourCodes.get(i));
-                        n.setStyle("-fx-pie-colour: " + colourCodes.get(i) + ";");
-                        System.out.println("-fx-pie-colour: " + colourCodes.get(i) + ";");
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-//                Node n = myPieChart.lookup(".default-color0.chart-pie");
-//                n.setStyle("-fx-pie-colour: " + colourCodes.get(0) + ";");
-//                n = myPieChart.lookup(".default-color1.chart-pie");
-//                n.setStyle("-fx-pie-colour: " + colourCodes.get(1) + ";");
-//                n = myPieChart.lookup(".default-color2.chart-pie");
-//                n.setStyle("-fx-pie-colour: " + colourCodes.get(2) + ";");
-//                n = myPieChart.lookup(".default-color2.chart-pie");
-//                n.setStyle("-fx-pie-colour: " + colourCodes.get(3) + ";");
-//                n = myPieChart.lookup(".default-color2.chart-pie");
-//                n.setStyle("-fx-pie-colour: " + colourCodes.get(4) + ";");
+                Node n = myPieChart.lookup(".data0.chart-pie");
+                n.setStyle("-fx-pie-color: " + colourCodes.get(0));
+                n = myPieChart.lookup(".data1.chart-pie");
+                n.setStyle("-fx-pie-color: " + colourCodes.get(1));
+                n = myPieChart.lookup(".data2.chart-pie");
+                n.setStyle("-fx-pie-color: " + colourCodes.get(2));
+                n = myPieChart.lookup(".data3.chart-pie");
+                n.setStyle("-fx-pie-color: " + colourCodes.get(3));
+                n = myPieChart.lookup(".data4.chart-pie");
+                n.setStyle("-fx-pie-color: " + colourCodes.get(4));
 
             } catch (Exception e) {
                 e.printStackTrace();
+            } finally {
+                myPieChart.setLegendVisible(false);
+                myPieChart.setTitle("Time spent on today's top 5 categories");
             }
-            // here you'll probably add the color code work (i think). make sure you do it in a try/catch.
-            // you'll need to do a sql query to get the colours from the categories table 
-            // IDK why it doesnt work, the data is coming in properly and ive changed it in pie terms as well.. 
-
-            myPieChart.setLegendVisible(false);
-            // jeff todo : you need to change this title
-            myPieChart.setTitle("Time spent on today's top 5 categories");
 
         } catch (SQLException e) {
             e.printStackTrace();
