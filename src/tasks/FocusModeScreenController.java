0f26795d5038d3a820140519595a762c9e22690c
Tim Hor <timothy.hor@outlook.com>
2019-11-15 11:15:22 +1100

    Hide completed tasks from deep focus dropdown

--- src/tasks/FocusModeScreenController.java
+++ src/tasks/FocusModeScreenController.java
@@ -114,7 +114,7 @@ public class FocusModeScreenController {
 
         //we want to populate the combo box with name entries from the database
         try {
-            ResultSet rs = Database.getResultSet("SELECT title FROM tasks");
+            ResultSet rs = Database.getResultSet("SELECT title FROM tasks WHERE completed = '0'");
             while (rs.next()){
                 taskDropdown.getItems().addAll(rs.getString("title"));
             }
